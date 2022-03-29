8a06b17e648d134fcdab5b1b8e0234b7d5c9c3cb
jtaulman <jtaulman23@gmail.com>
2022-03-29 00:12:26 -0400

    Step 2

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -28,6 +28,9 @@ let map = L.map('mapid', {
     layers: [streets]
 })
 
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     // This function returns the style data for each of the earthquakes we plot on
